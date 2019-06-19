b07719ad9826d2727bdb7ecedf49d364ce177c4e
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-06-19 19:10:35 +0500

    Session Exceptions

---
+++ src/Exception/StorageException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is a part of "comely-io/sessions" package.
+ * https://github.com/comely-io/sessions
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comely-io/sessions/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\Sessions\Exception;
+
+/**
+ * Class StorageException
+ * @package Comely\Sessions\Exception
+ */
+class StorageException extends SessionsException
+{
+}
