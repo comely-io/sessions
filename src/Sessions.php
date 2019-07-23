d01218a8e9dfa290ca00bcde7ab32401b6d9a523
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-07-23 13:14:44 +0500

    1.0.11

--- src/Sessions.php
+++ src/Sessions.php
@@ -24,9 +24,9 @@ use Comely\Sessions\Storage\SessionStorageInterface;
 class Sessions
 {
     /** string Version (Major.Minor.Release-Suffix) */
-    public const VERSION = "1.0.10";
+    public const VERSION = "1.0.11";
     /** int Version (Major * 10000 + Minor * 100 + Release) */
-    public const VERSION_ID = 10010;
+    public const VERSION_ID = 10011;
 
     /** @var SessionStorageInterface */
     private $storage;
